3afc175061bdfe64baf1a8bd87f2d5fc3e347da7
Julian Feliciano <julsfelic@gmail.com>
2016-03-02 17:49:02 -0700

    Adds basic validations to the User model
    Added a test and basic validations for username and email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @user = User.find(session[:user_id]) if session[:user_id]
+    @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true
+
+  validates :username, presence: true, length: { minimum: 6 }, uniqueness: true
+  validates :email, presence: true, format: { with: /@/ }, uniqueness: true
 end
