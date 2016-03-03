a514a1d8b9ed689bb0736c27f95d202fe9cc4ebe
Julian Feliciano <julsfelic@gmail.com>
2016-03-03 16:38:28 -0700

    Allows a registered user to login
    When the user logins they are directed to their dashboard. Changed
    the user controller to use current user instead of find a user based
    on the param that is passed in. This iteration does not check for sad
    a sad path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root to: "static_pages#landing"
 
-  resources :albums, only: [:index]
+  resources :albums,  only: [:index]
   resources :artists, only: [:index, :show], param: :artist_name
-  
+
   resources :users, only: [:new, :create]
   resources :carts, only: [:index, :create, :destroy, :update]
 
-  get "/cart", to: "carts#index", as: "user_cart"
   get "/dashboard", to: "users#show"
-  get "/:genre", to: "genres#show", as: "genre"
 
+  get "/login",     to: "sessions#new"
+  post "/login",    to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  get "/cart",      to: "carts#index", as: "user_cart"
+  get "/:genre",    to: "genres#show", as: "genre"
 end
---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "User can login" do
+  scenario "they see their dashboard" do
+    user = FactoryGirl.create(:user)
+
+    visit root_path
+    click_link "Login"
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    expect(current_path).to eq(dashboard_path)
+
+    within(".user") do
+      expect(page).to have_content(user.username)
+      expect(page).to have_content(user.email)
+    end
+
+    within(".site-header") do
+      expect(page).to_not have_link("Login")
+      expect(page).to have_link("Logout")
+    end
+  end
+end
