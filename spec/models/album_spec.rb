ed160499309731bde7ddf17d0bfc1bcb13f5f174
Julian Feliciano <julsfelic@gmail.com>
2016-03-04 19:38:19 -0700

    Adds validations to Album model
    Added some presence validations to the Album model along with
    testing the relationships that the Album model has.

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Album, type: :model do
+  it { should belong_to(:genre) }
+  it { should belong_to(:artist) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:price) }
+end
