2e8d960a4feff50d1680605b0fa54e5a9a174839
Julian Feliciano <julsfelic@gmail.com>
2016-03-06 10:49:11 -0700

    Add styling to artist page along with slug for url
    Lots of styling refactoring.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,6 +4,6 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find_by(name: params[:artist_name])
+    @artist = Artist.find_by(slug: params[:artist_name])
   end
 end
---
+++ spec/features/user_can_view_an_artist_page_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.feature "User can view an artist page" do
+  scenario "they see all albums associated with artist" do
+    artist = Artist.create(name: "Miles Davis", slug: "miles-davis")
+    album_1 = artist.albums.create(
+      title: "Bitches Brew",
+      description: "An album by Miles Davis",
+      image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/7/72/Bitches_brew.jpg/220px-Bitches_brew.jpg",
+      release_year: 1970,
+      price: 1500
+    )
+    album_2 = artist.albums.create(
+      title: "Kind of Blue",
+      description: "Another album by Miles Daivs",
+      image_url: "https://upload.wikimedia.org/wikipedia/en/9/9c/MilesDavisKindofBlue.jpg",
+      release_year: 1959,
+      price: 1700
+    )
+
+    visit albums_path
+    click_link "Miles Davis", match: :first
+
+    expect(current_path).to eq("/artists/miles-davis")
+    expect(page).to have_content("MILES DAVIS")
+    expect(page).to have_content(album_1.title)
+    expect(page).to have_content(album_2.title)
+  end
+end
