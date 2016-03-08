630dfcb2c298f3194700f91e058ac36fdf33bf97
Julian Feliciano <julsfelic@gmail.com>
2016-03-08 13:48:41 -0700

    Enables credit card processessing through stripe
    Added stripe to our order page. Currently using the test api keys
    since I see no reason for us to have to use the production api keys
    for this app.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,34 @@ class OrdersController < ApplicationController
     if current_user
       @order = Order.new
       @order_albums = CompleteOrder.create_order_albums(session[:cart])
+      @publishable_key = ENV['PUBLISHABLE_KEY']
     else
       redirect_to login_path
     end
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = Order.new(user_id: current_user.id, total: params[:total])
 
     if @order.save
+      @amount = params[:total]
+
+      customer = Stripe::Customer.create(
+        email:  params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: "Rails Stripe customer",
+        currency: "usd"
+      )
+
+    # rescue Stripe::CardError => each
+    #   flash[:error] = e.message
+    #   redirect_to new_charge_path
+
       session[:order_id] = @order.id
       session[:cart].clear
       redirect_to @order
