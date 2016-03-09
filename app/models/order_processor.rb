ec1e816175d1bb1ace43a7881135986c45501351
Scott Firestone <scott.firestone.dev@gmail.com>
2016-03-09 11:42:15 -0700

    Refactor checkouts
    Renamed CompleteOrder PORO to OrderProcessor. Methods also renamed for readability.
    Extracted logic from orders controller to OrderProcessor.

--- app/models/order_processor.rb
+++ app/models/order_processor.rb
@@ -1,6 +1,6 @@
-class CompleteOrder
+class OrderProcessor
 
-  def self.create_order_albums(cart)
+  def self.prepare_order_albums(cart)
     @order_albums = cart.map do |cart_item|
       OrderAlbum.new(album_id: cart_item[0].to_i, quantity: cart_item[1])
     end
@@ -12,10 +12,12 @@ class CompleteOrder
     end
   end
 
-  def self.finalize_order_albums(cart, order)
-    cart.map do |cart_item|
+  def self.finalize_order(session, order)
+    session[:order_id] = order.id
+    session[:cart].map do |cart_item|
       OrderAlbum.create(album_id: cart_item[0].to_i, quantity: cart_item[1],
       order_id: order.id)
     end
+    session[:cart].clear
   end
 end
